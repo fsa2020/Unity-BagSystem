c51a6324910405091ff30a0eeac3ae2f7e3b4555
fsa <1316240582@qq.com>
2024-04-25 15:58:31 +0800

    first commit

---
+++ Assets/Bag.cs
@@ -0,0 +1,479 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class Bag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject BtnScroll;
+    public GameObject ItemScroll;
+
+    public enum ItemType {
+        All,
+        Equip,
+        Food,
+
+    }
+
+
+    public List<ItemType> Tabs = new List<ItemType>() {
+        ItemType.All,
+        ItemType.Equip,
+        ItemType.Food,
+    };
+
+    List<bagDataSim> items;
+    int curTabIndex = 0;
+    void Awake()
+    {
+        InitBagTabs();
+    }
+    void Start()
+    {
+        items = GetItemsInBag();
+
+        SetTab();
+    }
+
+    List<Transform> tabBtns = new List<Transform>();
+    List<Transform> itemUIs = new List<Transform>();
+    List<bagDataSim> itemDatas = new List<bagDataSim>();
+
+    void InitBagTabs() {
+        for (int i = 0; i < Tabs.Count; i++)
+        {
+            GameObject tabBtn = Instantiate(Resources.Load("Prefabs/TabButton")) as GameObject;
+            Transform tabContent = BtnScroll.transform.Find("Viewport/Content");
+            tabBtn.transform.SetParent(tabContent);
+            int index = i;
+            tabBtn.GetComponent<Button>()?.onClick.AddListener(() => {
+                SetTab(index);
+            });
+
+            tabBtn.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = Enum.GetNames(typeof(ItemType))[index];
+
+            tabBtns.Add(tabBtn.transform);
+        }
+    }
+    void SetTab(int tabIndex = 0) {
+
+        curTabIndex = tabIndex;
+        Debug.Log("curTabIndex ：" + curTabIndex);
+        RefreshBtnList();
+        RefreshItemList();
+
+    }
+    void RefreshBtnList()
+    {
+        for (int i = 0; i < tabBtns.Count; i++)
+        {
+            tabBtns[i].GetComponent<Button>().interactable = (i != curTabIndex);
+        }
+    }
+    void RefreshItemList()
+    {
+        ClearItemList();
+        Transform itemContent = ItemScroll.transform.Find("Viewport/Content");
+        foreach (var itemData in items)
+        {
+
+            if (Tabs[curTabIndex] != ItemType.All && itemData.type != Tabs[curTabIndex]) continue;
+
+            GameObject item = Instantiate(Resources.Load("Prefabs/BagItem")) as GameObject;
+            item.transform.SetParent(itemContent);
+
+            SetItem(item, itemData);
+
+            itemUIs.Add(item.transform);
+            itemDatas.Add(itemData);
+        }
+
+
+        Debug.Log("cur type item num ：" + itemUIs.Count);
+
+    }
+    void SetItem(GameObject item,bagDataSim data) 
+    {
+        item.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "id " + data.id;
+    }
+    void ClearItemList()
+    {
+        StopMoving();
+        itemUIs.Clear();
+        itemDatas.Clear();
+        Transform itemContent = ItemScroll.transform.Find("Viewport/Content");
+        for (int i = 0; i < itemContent.childCount; i++)
+        {
+            Transform child = itemContent.GetChild(i);
+            Destroy(child.gameObject);
+        }
+    }
+
+    // click and long press handler and move animation while long press
+    void Update()
+    {
+        ClickAndLongPressHandler();
+        BagItemMovingHandler();
+    }
+
+
+    bool isPressing = false;
+    bool isDragBegin = false;
+    bool isDragging = false;
+    public float longPressTime = 0.6f;
+    float pressTime = 0.0f;
+
+    Vector2 beginPos = Vector2.zero;
+    GameObject clickItem = null;
+    int clickItemIndex = -1;
+
+    float clickCancelOffsetLen = 5;
+
+    void ClickAndLongPressHandler() {
+        if (isPressing)     pressTime += Time.deltaTime;
+        else                pressTime = 0.0f;
+
+        isDragBegin = pressTime > longPressTime && !isDragging;
+        
+        isDragging = pressTime > longPressTime;
+   
+        ItemScroll.GetComponent<ScrollRect>().enabled = !isDragging;
+
+
+        if (!isPressing && !isItemMoving && Input.GetMouseButtonDown(0))
+        {
+            OnClickDown(Input.mousePosition);
+        }
+        else if (isPressing && !isDragging && Vector2.Distance(beginPos, Input.mousePosition)>= clickCancelOffsetLen) 
+        {
+            OnClickUp(Input.mousePosition);
+        }
+        else if (isPressing && Input.GetMouseButtonUp(0))
+        {
+            OnClickUp(Input.mousePosition);
+        }
+        else if (isPressing && isDragging)
+        {
+            OnDragging(Input.mousePosition);
+        }
+
+    }
+
+    bool InItemBox(Vector2 pos, GameObject item) {
+        var size = item.GetComponent<RectTransform>().rect.size;
+        Vector2 itemPos = item.transform.position;
+        Vector2 offset = pos - itemPos;
+        return Math.Abs(offset.x) < size.x * 0.45 && Math.Abs(offset.y) < size.y * 0.45;
+    }
+ 
+    void OnClickDown(Vector2 pos)
+    {
+        Debug.Log("OnClickDown" + pos);
+        isPressing = true;
+        beginPos = pos;
+
+       for (int i = 0; i < itemUIs.Count; i++)
+       {
+            Transform item = itemUIs[i];
+            if (InItemBox(pos, item.gameObject)) {
+                clickItem = item.gameObject;
+                clickItemIndex = i;
+                break;
+            }
+        }
+    }
+
+
+    void OnClickUp(Vector2 pos )
+    {
+        Debug.Log("OnClickUp" + pos);
+        isPressing = false;
+
+        if (isDragging) 
+        {
+            PlayResetPos();
+            OnDragOver();
+        }
+        else if (clickCancelOffsetLen > Vector2.Distance(beginPos, pos)) 
+        {
+            Debug.Log("(todo add item script) Click item :"+clickItemIndex+" -- "+clickItem);
+        }
+        else 
+        { 
+            Debug.Log("Click Cancel"); 
+        }
+
+        clickItem = null;
+        clickItemIndex = -1;
+    }
+
+    void OnDragOver()
+    {
+        if (clickItem == null) return;
+        //clickItem.transform.localScale = Vector3.one;
+        ClearItemMask();
+        Debug.Log("Drag end");
+    }
+
+    void OnDragStart() 
+    {
+        Debug.Log("Drag start");
+        SaveInitPos();
+        CreateItemMask();
+    }
+
+    float moveWaitTime = 0;
+    public float moveWaitTimeMax = 0.6f;
+    void OnDragging(Vector2 pos)
+    {
+        if (clickItem == null) return;
+
+        //Debug.Log("OnDragging" + pos);
+
+        if (isDragBegin) 
+        {
+            OnDragStart();
+        }
+
+        
+        clickItem.transform.position = pos;
+
+        int targetIndex = GetInsertIndex(pos);
+
+        //Debug.Log("isItemMoving"+isItemMoving);
+        //Debug.Log("targetIndex" + targetIndex);
+        //Debug.Log("clickItemIndex" + clickItemIndex);
+
+        if (!isItemMoving && targetIndex != -1 && targetIndex!=clickItemIndex)
+        {
+            moveWaitTime += Time.deltaTime;
+            //Debug.Log(moveWaitTime);
+            if (moveWaitTime > moveWaitTimeMax)
+            {
+                PlayInsertItemTo(clickItemIndex, targetIndex);
+            }
+        }
+        else
+        {
+            moveWaitTime = 0;
+        }
+    }
+
+
+    bool isItemMoving = false;
+    float moveTime = 0.0f;
+    public float moveAnimationTime = 0.4f;
+
+    List<Vector2> insertFlagPosList = new List<Vector2>();
+    List<Vector2> targetPosList = new List<Vector2>();
+    List<Vector2> orgPosList = new List<Vector2>();
+    List<Vector2> initPosList = new List<Vector2>();
+    void BagItemMovingHandler()
+    {
+        if (isItemMoving)
+        {
+
+            moveTime += Time.deltaTime;
+
+            for (int i = 0; i < itemUIs.Count; i++)
+            {
+                if (i == tempIndex && isDragging) continue;
+                float lerpX = Mathf.SmoothStep(orgPosList[i].x, targetPosList[i].x, moveTime / moveAnimationTime);
+                float lerpY = Mathf.SmoothStep(orgPosList[i].y, targetPosList[i].y, moveTime / moveAnimationTime);
+                Vector3 pos = itemUIs[i].transform.position;
+                pos.x = lerpX;
+                pos.y = lerpY;
+                itemUIs[i].transform.position = pos;
+            }
+            if (moveTime >= moveAnimationTime) StopMoving();
+
+        }
+        else
+        {
+            moveTime = 0.0f;
+        }
+
+        if (itemMask != null && clickItem != null)
+        {
+            itemMask.transform.position = clickItem.transform.position;
+        }
+
+    }
+
+    void SaveInitPos()
+    {
+        initPosList.Clear();
+        for (int i = 0; i < itemUIs.Count; i++)
+        {
+            Vector3 initPos = itemUIs[i].transform.position;
+            initPosList.Add(initPos);
+        }
+
+        insertFlagPosList.Clear();
+
+        for (int i = 0; i < itemUIs.Count; i++)
+        {
+            Vector3 posFlag = itemUIs[i].transform.position;
+            insertFlagPosList.Add(posFlag);
+        }
+    }
+
+    void SaveOrgPos()
+    {
+        orgPosList.Clear();
+        for (int i = 0; i < itemUIs.Count; i++)
+        {
+            Vector3 orgPos = itemUIs[i].transform.position;
+            orgPosList.Add(orgPos);
+        }
+    }
+
+    void SaveTargetPos()
+    {
+        targetPosList.Clear();
+        for (int i = 0; i < initPosList.Count; i++)
+        {
+            Vector3 posTarget = initPosList[i];
+            targetPosList.Add(posTarget);
+        }
+    }
+
+    public float insertTiggerDistance = 30.0f;
+    int GetInsertIndex(Vector2 pos)
+    {
+        if (isItemMoving) return -1;
+
+        //Debug.Log("GetInsertIndex insertFlagPosList.Count " + insertFlagPosList.Count);
+
+        for (int i = 0; i < insertFlagPosList.Count; i++)
+        {
+
+            if (Vector2.Distance(insertFlagPosList[i], pos) < insertTiggerDistance)
+            {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    int tempIndex = -1;
+    void PlayInsertItemTo(int holdingIndex, int targetIndex)
+    {
+        if (holdingIndex != targetIndex)
+        {
+            Debug.Log("PlayInsertItemTo: " + "holdingIndex " + holdingIndex + "; targetIndex " + targetIndex);
+
+            // set move org pos
+            SaveOrgPos();
+
+
+            var tempPos = orgPosList[holdingIndex];
+            orgPosList.RemoveAt(holdingIndex);
+            orgPosList.Insert(targetIndex, tempPos);
+            orgPosList[targetIndex] = clickItem.transform.position;
+
+
+            SaveTargetPos();
+
+            var tempUI = itemUIs[holdingIndex];
+            itemUIs.RemoveAt(holdingIndex);
+            itemUIs.Insert(targetIndex, tempUI);
+
+            var tempData = itemDatas[holdingIndex];
+            itemDatas.RemoveAt(holdingIndex);
+            itemDatas.Insert(targetIndex, tempData);
+
+            moveTime = 0.0f;
+            isItemMoving = true;
+
+            tempIndex = targetIndex;
+        }
+
+    }
+    void PlayResetPos()
+    {
+        Debug.Log("PlayResetPos: ");
+
+        // set move org pos
+
+        SaveOrgPos();
+
+        SaveTargetPos();
+        moveTime = 0.0f;
+        isItemMoving = true;
+
+    }
+
+
+    GameObject itemMask = null;
+    void CreateItemMask()
+    {
+        if (clickItem == null) return;
+
+        itemMask = Instantiate(Resources.Load("Prefabs/BagItem")) as GameObject;
+        itemMask.transform.SetParent(ItemScroll.transform);
+        itemMask.transform.position = clickItem.transform.position;
+        itemMask.transform.localScale = Vector3.one * 1.1f;
+        SetItem(itemMask, itemDatas[clickItemIndex]);
+
+    }
+    void ClearItemMask()
+    {
+        if (itemMask != null) Destroy(itemMask.gameObject);
+        itemMask = null;
+    }
+
+    void StopMoving()
+    {
+        moveTime = 0.0f;
+        isItemMoving = false;
+
+        if (tempIndex != -1 && clickItemIndex != -1)
+        {
+            clickItemIndex = tempIndex;
+            tempIndex = -1;
+        }
+    }
+
+
+
+    // 模拟背包的数据
+    List<bagDataSim> GetItemsInBag() {
+        List<bagDataSim> items = new List<bagDataSim>();
+        int simId = 0;
+        foreach (ItemType t in Enum.GetValues(typeof(ItemType)))
+        {
+            if (t == ItemType.All) continue;
+
+            int num = UnityEngine.Random.Range(2, 2);
+            for (int i = 0; i < num; i++)
+            {
+                bagDataSim data = new bagDataSim();
+                data.type = t;
+                data.id = simId;
+                data.amount = 1;
+                simId++;
+
+                items.Add(data);
+            }
+
+        }
+
+        return items;
+    }
+
+
+    public struct bagDataSim 
+    {
+        public int id;
+        public ItemType type;
+        public int amount;
+
+    }
+}
